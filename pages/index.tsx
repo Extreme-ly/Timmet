113f2fea2c97c7a3f4fd8be31259e98c4a7acd8c
Arihant <jainarti220@gmail.com>
2022-03-24 23:39:55 +0530

    landing page, setup

---
+++ pages/index.tsx
@@ -0,0 +1,79 @@
+import { Box, Button, IconButton, SpeedDial, SpeedDialAction, Typography } from '@mui/material'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import AppleIcon from '@mui/icons-material/Apple';
+import AndroidIcon from '@mui/icons-material/Android';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+import '@fontsource/roboto/100.css';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+import anime from 'animejs'
+import { useEffect, useRef } from 'react'
+
+function TitleDivider() {
+  return (
+    <Box sx={{ width: "24rem", height: 2, background: "#EEEEEE", position: 'absolute', ml: 29 }}></Box>
+  )
+}
+
+const Home: NextPage = () => {
+  useEffect(() => {
+    anime({
+      targets: '.speed-dial',
+      translateY: -20,
+      direction: 'alternate',
+      duration: 1000,
+      easing: 'easeInOutExpo',
+      loop: true
+    })
+  }, [])
+  const actions = [
+    { icon: <AppleIcon />, name: 'Apple Store' },
+    { icon: <AndroidIcon />, name: 'Play Store' },
+    { icon: <TwitterIcon />, name: 'Our Twitter' },
+    { icon: <InstagramIcon />, name: 'Our Insta Page' },
+  ];
+
+  return (
+    <div>
+      <Head>
+        <title>Timmet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box sx={{ height: "400px", display: 'grid', placeItems: 'center' }}>
+        <Typography component='div' variant='h2' sx={{ letterSpacing: '1em' }} className="title">
+          Timmet.
+          <TitleDivider />
+          <Typography variant='body1' sx={{ mt: 3, ml: 15 }}>
+            Lorem Ipsum ya di ya da
+          </Typography>
+        </Typography>
+        <IconButton size='large' sx={{ position: 'absolute', right: 0 }}>
+        <ArrowForwardIcon fontSize="inherit" />
+      </IconButton>
+      </Box>  
+
+      <SpeedDial
+        ariaLabel="SpeedDial"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        className='speed-dial'
+     
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+    </div>
+  )
+}
+
+export default Home
